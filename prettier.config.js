b2d3624eb5e73dbd46ed11f84ff3735a003b8016
leroy <gmail@leroytop.com>
2025-06-25 14:34:40 +0800

    chore: 添加 prettier-plugin-tailwindcss 依赖及配置

--- prettier.config.js
+++ prettier.config.js
@@ -1,10 +1,13 @@
 /** @type {import('prettier').Options} */
-const prettierConfig =  {
+const prettierConfig = {
   singleQuote: true,
   trailingComma: 'all',
   printWidth: 100,
   proseWrap: 'never',
   endOfLine: 'lf',
+  plugins: ['prettier-plugin-tailwindcss'],
+  tailwindStylesheet: './src/app.css',
+  tailwindFunctions: ['classNames'],
 };
 
 export default prettierConfig
